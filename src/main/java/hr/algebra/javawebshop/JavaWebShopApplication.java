03580eb45d008859a29f0df6611d8bf77884a078
enoo298 <enwerinho@gmail.com>
2025-04-30 15:05:36 +0200

    init

---
+++ src/main/java/hr/algebra/javawebshop/JavaWebShopApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.javawebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/algebra/javawebshop/JavaWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.javawebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
